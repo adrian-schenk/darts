1731a3c903306a1e74851f8c73ee922ec979201c
FearXenon <118049326+FearXenon@users.noreply.github.com>
2026-02-14 16:29:38 +0100

    format code

--- _backend/src/api/api.controller.ts
+++ _backend/src/api/api.controller.ts
@@ -1,10 +1,8 @@
-
 import { Controller, Get } from '@nestjs/common';
 import { ApiService } from './api.service';
 
 @Controller('api')
 export class ApiController {
-
   constructor(private readonly apiService: ApiService) {}
 
   @Get('/test')
