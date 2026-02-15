8d2e6bb81ed926b6548d52fb8382af9b0e0dcd08
FearXenon <118049326+FearXenon@users.noreply.github.com>
2026-02-15 20:45:03 +0100

    Add pinia-plugin-persistedstate dependency and update axios and qs versions; enhance Tailwind CSS configuration with color extensions

--- _frontend/tailwind.config.js
+++ _frontend/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 import animations from 'tailwindcss-animate';
+import colors from 'tailwindcss/colors';
 
 export default {
   content: [
@@ -7,7 +8,13 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        easy: '#22c55e',
+        medium: '#f59e0b',
+        hard: '#ef4444',
+      },
+    },
   },
   plugins: [animations],
 }
